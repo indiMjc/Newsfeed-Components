37b0226931146e80af45a0fb616973d491b47b6f
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-02 16:28:02 -0400

    span fixed and added new article

--- Article/Article.js
+++ Article/Article.js
@@ -88,6 +88,26 @@ const data = [
   }
 ];
 
+const newArt =
+  {
+    title: 'My new article',
+    date: 'Oct 2, 2019',
+    firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
+          hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
+          Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
+
+    secondParagraph: `Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor, hodor. Hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor;
+          hodor hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - HODOR hodor, hodor hodor hodor! Hodor, hodor. Hodor. Hodor, HODOR
+          hodor, hodor hodor, hodor, hodor hodor. Hodor hodor - hodor - hodor... Hodor hodor hodor hodor hodor hodor hodor?! Hodor
+          hodor - hodor hodor hodor. Hodor. Hodor hodor... Hodor hodor hodor hodor hodor? `,
+
+    thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
+          Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
+          Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  };
+
+data.push(newArt);
+
 const articles = document.querySelector(".articles");
 
 function createComponent(title, date, firstP, secondP, thirdP) {
@@ -111,10 +131,12 @@ function createComponent(title, date, firstP, secondP, thirdP) {
   newTitle.classList.add("date");
   newExpButton.classList.add("expandButton");
   //populate
-  newTitle.textContent = date;
+  newTitle.textContent = title;
+  newDate.textContent = date;
   newFirstP.textContent = firstP;
   newSecondP.textContent = secondP;
   newThirdP.textContent = thirdP;
+  newExpButton.textContent = "\u25BC";
   //event listener for span
   newExpButton.addEventListener("click", () => {
     article.classList.toggle("article-open")
@@ -126,28 +148,3 @@ function createComponent(title, date, firstP, secondP, thirdP) {
 data.forEach(info => {
   articles.appendChild(createComponent(info.title, info.date, info.firstParagraph, info.secondParagraph, info.thirdParagraph));
 });
-
-/* Step 1: Create a function that creates a component. You will want your component to look like the template below: 
-  
-  <div class="article">
-    <h2>{title of the article}</h2>
-    <p class="date">{date of the article}</p>
-
-    {three separate paragraph elements}
-
-    <span class='expandButton'></span>
-  </div>
-
-  Hint: You will need to use createElement more than once here!
-
-  Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
-
-  Step 2: Add an event listener to the expandButton span. This event listener should toggle the class 'article-open' on the 'article' div.
-
-  Step 3: return the entire component.
-
-  Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
-
-  Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
-
-*/
